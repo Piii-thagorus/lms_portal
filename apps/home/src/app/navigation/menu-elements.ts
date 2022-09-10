5d03b8cb7cab974dfb1ccef936aec11468651eb4
Lebogang Molapo <lmolapo021@student.wethinkcode.co.za>
2022-09-10 16:22:11 +0200

    Added new menu elements

---
+++ apps/home/src/app/navigation/menu-elements.ts
@@ -0,0 +1,61 @@
+
+interface chip{
+    value: number;
+    color : string;
+}
+
+
+export interface menu {
+    name : string;
+    icon : string;
+    link : boolean | string ;
+    open : boolean;
+    chip ?: chip
+    sub ?: menu
+
+}
+
+export const MENUS : menu[] = [
+
+    {
+        name : "Work/Learnership",
+        icon : "work",
+        link : false,
+        open : false,
+    },
+
+    {
+        name : "Bursary",
+        icon : "payments",
+        link : false,
+        open : false
+    },
+
+    {
+        name : "FET",
+        icon : "school",
+        link : false,
+        open : false
+    },
+
+    {
+        name : "Calendar",
+        icon : "calendar_month",
+        link : false,
+        open : false
+    },
+
+    {
+        name : "Sports",
+        icon : "sports",
+        link : false,
+        open : false
+    },
+
+    {
+        name : "Volunteer",
+        icon : "volunteer_activism",
+        link : false,
+        open : false
+    },
+]
