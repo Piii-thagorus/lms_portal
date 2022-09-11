60e49bbbcfc3a1f7103111fb14aa91388bb4cd9b
Lebogang Molapo <lmolapo021@student.wethinkcode.co.za>
2022-09-11 13:46:48 +0200

    LMSP-7 configure AuthModule with credentials

--- apps/login/src/app/app.module.ts
+++ apps/login/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthModule} from "@auth0/auth0-angular";
+import {environment} from "../environments/environment";
 
 @NgModule({
  declarations: [AppComponent],
@@ -9,8 +11,14 @@ import { AppComponent } from './app.component';
    BrowserModule,
    RouterModule.forRoot([{
      path: '',
-     loadChildren: () => import('./remote-entry/entry.module').then(m => m.RemoteEntryModule)
+     loadChildren: () =>
+         import('./remote-entry/entry.module').
+         then(m => m.RemoteEntryModule)
    }], { initialNavigation: 'enabledBlocking' }),
+
+   AuthModule.forRoot({
+       ...environment.auth
+   })
  ],
  providers: [],
  bootstrap: [AppComponent],
