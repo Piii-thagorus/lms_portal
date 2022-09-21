4a5f022993549ab47bdbfe420e429ee522fac085
Lebogang Molapo <lmolapo021@student.wethinkcode.co.za>
2022-09-21 16:36:13 +0200

    LMSP-7 added main route for login module

---
+++ apps/login/src/app/remote-entry/app.routes.ts
@@ -0,0 +1,11 @@
+import {Routes} from "@angular/router";
+import {RemoteEntryComponent} from "./remote-entry.component";
+
+export const LOGIN_ROUTES : Routes = [
+    {
+        path: '',
+        component: RemoteEntryComponent
+    }
+
+];
+
