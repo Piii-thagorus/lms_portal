fbfe5adeb65d18a0460fa85416b487c38d1a9af0
Lebogang Molapo <lmolapo021@student.wethinkcode.co.za>
2022-10-03 22:10:59 +0200

    LMSP-10 added angular material for styling

--- apps/new-api/src/app/remote-entry/entry.module.ts
+++ apps/new-api/src/app/remote-entry/entry.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
 import { RemoteEntryComponent } from './entry.component';
 import {NewsApiService} from "../services/news.api.service";
 import {NewsAPIComponent} from "../components/news-apicomponent/news-a-p-i.component";
 import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [RemoteEntryComponent, NewsAPIComponent ],
@@ -18,6 +25,14 @@ import {HttpClientModule} from "@angular/common/http";
         component: RemoteEntryComponent,
       },
     ]),
+//    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [NewsApiService],
 })
