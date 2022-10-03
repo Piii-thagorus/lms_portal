dde142f05e838ee7ccfa60b499d6eca03b7d4403
Lebogang Molapo <lmolapo021@student.wethinkcode.co.za>
2022-10-03 18:39:51 +0200

    LMSP-7 fixed path

--- apps/home/src/app/app.routes.ts
+++ apps/home/src/app/app.routes.ts
@@ -6,7 +6,6 @@ export const ROUTES : Routes = [
     {
         path: '',
         outlet: 'topMenu',
-        pathMatch: 'full',
         loadChildren: () => import(
             'login/Module'
             ).then((m) => m.RemoteEntryModule)
