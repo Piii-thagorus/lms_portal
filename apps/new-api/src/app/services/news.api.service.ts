d163252da1af03251d108018c1549b32c8f840c9
Lebogang Molapo <lmolapo021@student.wethinkcode.co.za>
2022-10-05 22:18:16 +0200

    LMSP-10 added method that gets articles by category

--- apps/new-api/src/app/services/news.api.service.ts
+++ apps/new-api/src/app/services/news.api.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -8,11 +9,10 @@ export class NewsApiService{
 
     private API_KEY : string = '6f8a999cbf014e9a9685cc200320c60d'
 
-    constructor(private http: HttpClient) {
-
-    }
+    constructor(private http: HttpClient) {}
 
     initSources(){
+        
         return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.API_KEY);
     }
 
@@ -20,7 +20,11 @@ export class NewsApiService{
         return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.API_KEY);
     }
 
-    getArticlesByID(source: String){
+    getArticlesByID(source: string){
         return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.API_KEY);
+    }
+
+    getArticlesByCategory(category: string){
+       return this.http.get(`https://newsapi.org/v2/top-headlines?country=za&category=${category}&apiKey=${this.API_KEY}`)
     }
 }
