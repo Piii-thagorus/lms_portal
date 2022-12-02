a83efe2ec1419ccbf7a5de5ddd9208a357525eb5
Piii-thagorus <molapo.lm@outlook.com>
2022-12-02 20:09:47 +0200

    LMSP-9 added angular calendar

--- apps/calendar/src/app/remote-entry/entry.module.ts
+++ apps/calendar/src/app/remote-entry/entry.module.ts
@@ -1,21 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import {FullCalendarModule} from "@fullcalendar/angular";
+import { FormsModule } from '@angular/forms';
+import { CalendarModule, DateAdapter } from 'angular-calendar'
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns'
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap'
 import { RemoteEntryComponent } from './entry.component';
 import { CalendarComponent } from '../calendar-components/calendar/calendar.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [RemoteEntryComponent, CalendarComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    NgbModalModule,
+    MatCardModule,
+    MatIconModule,
+    NgbModule,
+    FlexLayoutModule,
     RouterModule.forChild([
       {
         path: '',
         component: RemoteEntryComponent,
       },
     ]),
-    FullCalendarModule
+
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   exports: [RemoteEntryComponent],
