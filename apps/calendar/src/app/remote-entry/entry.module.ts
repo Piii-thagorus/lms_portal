860fe8630aa66d293b2ea64d6307799e83d7f008
Piii-thagorus <molapo.lm@outlook.com>
2022-12-05 08:31:30 +0200

    LMSP-9 imported ReactiveForms and font-awesome modules

--- apps/calendar/src/app/remote-entry/entry.module.ts
+++ apps/calendar/src/app/remote-entry/entry.module.ts
@@ -11,16 +11,26 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import {FlatpickrModule} from 'angularx-flatpickr'
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [RemoteEntryComponent, CalendarComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     NgbModalModule,
     MatCardModule,
     MatIconModule,
     NgbModule,
+    FontAwesomeModule,
+    FlatpickrModule.forRoot(),
     FlexLayoutModule,
     RouterModule.forChild([
       {
@@ -34,7 +44,12 @@ import { FlexLayoutModule } from "@angular/flex-layout";
       useFactory: adapterFactory
     })
   ],
-  providers: [],
+  providers: [DatePipe],
   exports: [RemoteEntryComponent],
 })
-export class RemoteEntryModule {}
+export class RemoteEntryModule {
+
+    constructor(iconLibrary: FaIconLibrary){
+      iconLibrary.addIcons(faCalendar);
+    }
+}
